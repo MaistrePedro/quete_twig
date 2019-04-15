6da31c1e1dc9c8683032656616c3e647febdaa50
MaistrePedro <peterpendrag@gmail.com>
2019-04-15 20:36:35 +0200

    quete terminée

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__.'/../vendor/autoload.php';
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+$page = 'home';
+if (isset($_GET['p'])) {
+    $page = $_GET['p'];
+}
+$loader = new \Twig\Loader\FilesystemLoader('../src/View/');
+$twig = new \Twig\Environment($loader);
+
+if ($page === 'home') {
+    echo $twig->render('index.html.twig', ['products' => $products]);
+}
